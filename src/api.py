a5f3f383086a6b6757979b5ad5e4c3edab849d14
Tom@DESKTOP-3M3JSCQ <Tom.Helling1234@gmail.com>
2025-03-04 12:28:36 +0100

    Added more apii methods and implemented basic auth

--- src/api.py
+++ src/api.py
@@ -1,4 +1,7 @@
-"""This file provides a basic API to interact with an AI model."""
+"""
+This file provides a basic API to interact with an AI model.
+Collection: https://www.postman.com/postman-student-programs/ollama-api/
+"""
 
 from fastapi import FastAPI, Depends, HTTPException, Header
 import ollama
@@ -31,3 +34,48 @@ class APIManager:
             model="mistral", messages=[{"role": "user", "content": prompt}]
         )
         return {"response": response["message"]["content"]}
+    
+    @app.post("/create")
+    def create(self):
+        print("Success!")
+
+    @app.get("/tags")
+    def tags(self, Permission):
+        if Permission == True:
+            print("Success!")
+
+    @app.post("/show")
+    def show(self):
+        print("Success!")
+
+    @app.post("/copy")
+    def copy(self):
+        print("Success!")
+    
+    @app.delete("/delete")
+    def delete(self):
+        print("Success!")
+
+    @app.post("/pull")
+    def pull(self):
+        print("Success!")
+
+    @app.post("/push")
+    def push(self):
+        print("Success!")
+
+    @app.post("/embed")
+    def embed(self):
+        print("Success!")
+
+    @app.post("/ps")
+    def ps(self):
+        print("Success!")
+
+    @app.post("/chat")
+    def chat(self):
+        print("Success!")
+
+    @app.post("/version")
+    def version(self):
+        print("Success!")
